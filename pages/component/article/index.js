66905cf673d6358d1550d4a50f5b0b71eb3f0ae7
JavkC <549595297@qq.com>
2017-08-22 16:57:44 +0800

    init

--- pages/component/article/index.js
+++ pages/component/article/index.js
@@ -1,7 +1,13 @@
+var url = require('../../../utils/baseurl.js');
 var app = getApp()
 
 Page({
     data: {
+      rec: {
+        idx: 0, loading: false,
+      },
+      message:url.url,
+      tabidx: 0
     },
 
     onLoad: function () {
@@ -9,6 +15,16 @@ Page({
     },
 
     onShow: function() {
+      // if (!wx.getStorageSync('user')) {
+      //   wx.redirectTo({
+      //     //url: '../login/index'
+      //   });
+      //   return;
+      // }
+      this.init();
     },
+    init:function(){
+      console.info("init");
+    }
 })
 
