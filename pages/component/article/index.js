f771acb1cbeefb9d07a50cd204d89f22537167b3
JavkC <549595297@qq.com>
2017-08-23 09:03:21 +0800

    init

--- pages/component/article/index.js
+++ pages/component/article/index.js
@@ -1,12 +1,16 @@
 var url = require('../../../utils/baseurl.js');
+var Util = require('../../../utils/util.js');
 var app = getApp()
 
 Page({
     data: {
       rec: {
-        idx: 0, loading: false,
+        idx: 0, 
+        loading: false,
       },
-      message:url.url,
+      articles: [],
+      currentPage: 0,
+      isFinished: false,
       tabidx: 0
     },
 
@@ -15,16 +19,53 @@ Page({
     },
 
     onShow: function() {
-      // if (!wx.getStorageSync('user')) {
-      //   wx.redirectTo({
-      //     //url: '../login/index'
-      //   });
-      //   return;
-      // }
-      this.init();
+      this.init(1);
     },
-    init:function(){
+    /**
+     * 初始化文章列表
+     */
+    init:function(p){
+      Util.networkStatus();
+      let that = this;
+      var rec = this.data.rec;
+     
+      wx.request({
+        url: url.url +'/wx/recent',
+        method: 'POST',
+        data: {
+          p: p
+        },
+        header: {
+          'content-type': 'application/json',
+          'Cookie': 'b3log-latke=' + wx.getStorageSync('cookie')
+        },
+        success: function (res) {
+          if (res.errMsg == 'request:ok') {
+            console.info(res.data.articles)
+            rec.idx = 0;
+            rec.loading = true;
+            that.setData({
+              articles: that.data.articles.concat(res.data.articles),
+              rec:rec
+            })
+          }
+        },
+        fail: function (res) {
+          rec.idx = 0;
+          rec.loading = false;
+          that.setData({
+            articles: [],
+            rec: rec
+          })
+        }
+      })
+
       console.info("init");
+    },
+    switchtab:function(e){
+      var that = this;
+      var t = e.currentTarget.dataset.t;
+      this.setData({ tabidx: t });
     }
 })
 
